337c040e5343e82bb3101cf504853a11262f2e17
Ashish <ashishjadhav9900@gmail.com>
2023-11-10 19:07:06 +0530

    Updates ArrayImpOfStack

---
+++ test.c
@@ -0,0 +1,29 @@
+#include<stdio.h>
+#include<stdlib.h>
+struct Stack { 
+	int top; 
+	int capacity; 
+	int* array; 
+};
+
+struct Stack* createStack(int capacity) 
+{ 
+	struct Stack* stack = (struct Stack*)malloc(sizeof(struct Stack)); 
+	stack->capacity = capacity; 
+	stack->top = -1; 
+	stack->array = (int*)malloc(stack->capacity * sizeof(int));
+	 
+	return stack; 
+} 
+
+int main(){
+
+    struct Stack* stack;
+
+    
+
+    printf("%d",sizeof(100*sizeof(stack)));
+
+
+    return 0;
+}
