6db071fe6a48a42578050c5c4fcfc6b4b0e7537e
Ashish <ashishjadhav9900@gmail.com>
2023-11-10 10:58:14 +0530

    ArrayOfsatckUpdated

--- Chapter1/Stack/ArrayImplementationOfStack.c
+++ Chapter1/Stack/ArrayImplementationOfStack.c
@@ -1,16 +1,17 @@
+// C program for array implementation of stack 
 #include <limits.h> 
 #include <stdio.h> 
 #include <stdlib.h> 
 
-
+// A structure to represent a stack 
 struct Stack { 
 	int top; 
 	unsigned capacity; 
 	int* array; 
 }; 
 
-
-
+// function to create a stack of given capacity. It initializes size of 
+// stack as 0 
 struct Stack* createStack(unsigned capacity) 
 { 
 	struct Stack* stack = (struct Stack*)malloc(sizeof(struct Stack)); 
@@ -20,18 +21,19 @@ struct Stack* createStack(unsigned capacity)
 	return stack; 
 } 
 
+// Stack is full when top is equal to the last index 
 int isFull(struct Stack* stack) 
 { 
 	return stack->top == stack->capacity - 1; 
 } 
 
-
+// Stack is empty when top is equal to -1 
 int isEmpty(struct Stack* stack) 
 { 
 	return stack->top == -1; 
 } 
 
-
+// Function to add an item to stack. It increases top by 1 
 void push(struct Stack* stack, int item) 
 { 
 	if (isFull(stack)) 
@@ -40,7 +42,7 @@ void push(struct Stack* stack, int item)
 	printf("%d pushed to stack\n", item); 
 } 
 
-
+// Function to remove an item from stack. It decreases top by 1 
 int pop(struct Stack* stack) 
 { 
 	if (isEmpty(stack)) 
@@ -48,7 +50,7 @@ int pop(struct Stack* stack)
 	return stack->array[stack->top--]; 
 } 
 
-
+// Function to return the top from stack without removing it 
 int peek(struct Stack* stack) 
 { 
 	if (isEmpty(stack)) 
@@ -56,6 +58,7 @@ int peek(struct Stack* stack)
 	return stack->array[stack->top]; 
 } 
 
+// Driver program to test above functions 
 int main() 
 { 
 	struct Stack* stack = createStack(100); 
